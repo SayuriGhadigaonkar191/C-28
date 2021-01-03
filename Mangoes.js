adc1d07118f2eee103cdab5ecf49e1209f6553b8
SayuriGhadigaonkar191 <76142699+SayuriGhadigaonkar191@users.noreply.github.com>
2021-01-03 12:37:09 +0530

    Add files via upload

---
+++ Mangoes.js
@@ -0,0 +1,7 @@
+class mangoes extends BaseClass{
+    constructor(x,y,height,angle){
+      super(x,y,20,height,angle);
+      this.image = loadImage("sprites/mango.png");
+      Matter.Body.setAngle(this.body, angle);
+    }
+  }
